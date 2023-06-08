b07f73f939c6d24f774fe634e64b37a5ec15ea3d
rifkifathur <92842676+rifkifathur@users.noreply.github.com>
2023-06-08 13:33:19 +0700

    update

--- src/component/Intro/Intro.jsx
+++ src/component/Intro/Intro.jsx
@@ -1,16 +1,16 @@
 import './Intro.scss';
-
+import ProfilePict from './foto_profile_.jpg'
 const Intro = () => {
     return (
         <section className="intro_" id='home'>
                 <div className="col_left">
                     <h3>
                         <span>Hello, IM <span>Rifki Fathur</span></span>
-                        <span>Front End Programmer</span>
+                        <span>Web Developer</span>
                     </h3>
                 </div>
                 <div className="col_right">
-                    <img src="./assets/profile_pict.jpg" alt="profile_pict" />
+                    <img src={ProfilePict} alt="profile_pict" />
                 </div>
                 <footer>
                     <img src="./assets/3.svg" alt="footer" />
